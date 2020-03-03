769efa5af7ee165895d296a42a17d7e7affaaf2f
daipham2506 <daipham2506@gmail.com>
2020-03-03 11:34:30 +0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+
+
+app.get('/', (req,res) => {
+    return res.send('API running')
+})
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Sever started on port ${PORT}`)
+})
