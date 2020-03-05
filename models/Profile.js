3b2b0c3d9a27fa016addea3e70d8c00c51922265
daipham2506 <daipham2506@gmail.com>
2020-03-05 11:02:47 +0700

    Profile API Routes

--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ProfileSchema = new mongoose.Schema({
     user:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'users'
     },
     company:{
         type: String
